928c147f1f8354a37f956ace44b69e28ae71acea
Waelson Negreiros Nunes <waelson@MacBook-Pro-de-Waelson.local>
2024-03-18 19:32:02 -0300

    fix

--- internal/controller/application.go
+++ internal/controller/application.go
@@ -25,11 +25,14 @@ func (c *applicationController) Handler(w http.ResponseWriter, r *http.Request) 
 	w.Header().Set("Content-Type", "application/json")
 
 	if errors.Is(err, model.InvalidCepError) {
-		w.WriteHeader(http.StatusUnprocessableEntity)
+		//w.WriteHeader(http.StatusUnprocessableEntity)
+		http.Error(w, "invalid zipcode", http.StatusUnprocessableEntity)
 	} else if errors.Is(err, model.CepNotFoundError) {
-		w.WriteHeader(http.StatusNotFound)
+		//w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "can not find zipcode", http.StatusNotFound)
 	} else if errors.Is(err, model.InternalError) {
-		w.WriteHeader(http.StatusInternalServerError)
+		//w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "internal error", http.StatusInternalServerError)
 	} else {
 		if err := json.NewEncoder(w).Encode(response); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
